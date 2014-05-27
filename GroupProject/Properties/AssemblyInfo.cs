d47481017ded428b8208553796faa6f5a9afe07f
BalthazarAIE <justinl@aie.edu.au>
2014-05-27 13:59:14 -0700

    Second Commit
    Organization

--- GroupProject/Properties/AssemblyInfo.cs
+++ GroupProject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Blah")]
+[assembly: AssemblyTitle("GroupProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hewlett-Packard Company")]
-[assembly: AssemblyProduct("Blah")]
+[assembly: AssemblyProduct("GroupProject")]
 [assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0bcf9acd-11d2-4341-a63b-3bfafdfb714c")]
+[assembly: Guid("e54a9284-c5c0-486d-9f34-8f020d9047e4")]
 
 // Version information for an assembly consists of the following four values:
 //
