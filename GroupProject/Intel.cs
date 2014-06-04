c06425684f906b4960d480d308d8120d0b90ca46
Derek Potter <derekpotter@msn.com>
2014-06-04 10:29:59 -0700

    Lep, Intel Class
    basic intel class for now

---
+++ GroupProject/Intel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GroupProject
+{
+    class Intel : Sprites
+    {
+        #region Variables
+
+        public bool IsCaptured;
+
+        #endregion
+
+        public Intel(int w, int h, float s)
+        {
+            this.width = w;
+            this.height = h;
+            this.scale = s;
+        }
+
+        #region Draw and Update
+
+        public new void Update()
+        {
+            this.UpdatePosition();
+        }
+
+        #endregion
+    }
+}
